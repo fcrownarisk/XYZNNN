b4c1a555ba6eaf2f244bef648f5d594bff670dca
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-11-04 23:21:26 +0800

    Add files via upload

---
+++ donut/Array.ts
@@ -0,0 +1,3 @@
+type XYZ = Int8Array | Int16Array | Int32Array
+type t = Float32Array | Float64Array
+type fx = Uint8Array | Uint16Array | Uint32Array | Uint8ClampedArray
---
+++ fxt/w.c
@@ -0,0 +1,16 @@
+short abcdef(){
+   const a = 'w1';
+   const b = 'w2';
+   const c = 'w3';
+   const d = "w1'";
+   const e = "w2'";
+   const f = "w3'";
+}
+int bin(){
+   int a = "00001011";
+   int b = "00001100";
+   int c = "00001101";
+   int d = "00001110";
+   int e = "00001111";
+   int f = "00010000"; 
+}
